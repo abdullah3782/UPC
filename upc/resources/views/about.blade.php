b1ae23370245da41b46131c549d616635bbfb64b
abdullah3782 <abdullah.butt093@gmail.com>
2024-02-14 13:49:20 +0100

    first commit

---
+++ upc/resources/views/about.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Present Cards con Bootstrap</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+
+<div class="cards-container">
+    <div class="person">
+        <img class="logo" src="../../../img/avatar2.jpg" alt="Avatar 1">
+        <h4 class="name">Abdullah Waris</h4>
+        <div class="info">
+            <h6>Estudios: <span>FP-DAW</span></h6>
+            <p>Descripción: Programador Full Stack con 2 años de experiencia, programa en JS y PHP, usa Angular, React y Laravel como frameworks</p>
+        </div>
+    </div>
+    <div class="person">
+        <img class="logo" src="../../../img/avatar2.jpg" alt="Avatar 2">
+        <h4 class="name">Shah Zaib</h4>
+        <div class="info">
+            <h6>Estudios: <span>FP-DAW</span></h6>
+            <p>Descripción: Programador Full Stack con 2 años de experiencia, programa en JS y PHP, usa Angular, React y Laravel como frameworks</p>
+        </div>
+    </div>
+    <div class="person">
+        <img class="logo" src="../../../img/avatar2.jpg" alt="Avatar 3">
+        <h4 class="name">Pol Moreno</h4>
+        <div class="info">
+            <h6>Estudios: <span>FP-DAW</span></h6>
+            <p>Descripción: Programador Full Stack con 2 años de experiencia, programa en JS y PHP, usa Angular, React y Laravel como frameworks</p>
+        </div>
+    </div>
+    <div class="person">
+        <img class="logo" src="../../../img/avatar2.jpg" alt="Avatar 4">
+        <h4 class="name">Jan Viñas</h4>
+        <div class="info">
+            <h6>Estudios: <span>FP-DAW</span></h6>
+            <p>Descripción: Programador Full Stack con 2 años de experiencia, programa en JS y PHP, usa Angular, React y Laravel como frameworks</p>
+        </div>
+    </div>
+    <div class="person">
+        <img class="logo" src="../../../img/avatar2.jpg" alt="Avatar 5">
+        <h4 class="name">David Peludo</h4>
+        <div class="info">
+            <h6>Estudios: <span>FP-DAW</span></h6>
+            <p>Descripción: Programador Full Stack con 2 años de experiencia, programa en JS y PHP, usa Angular, React y Laravel como frameworks</p>
+        </div>
+    </div>
+    <div class="person">
+        <img class="logo" src="../../../img/avatar2.jpg" alt="Avatar 6">
+        <h4 class="name">Alex Calvo</h4>
+        <div class="info">
+            <h6>Estudios: <span>FP-DAW</span></h6>
+            <p>Descripción: Programador Full Stack con 2 años de experiencia, programa en JS y PHP, usa Angular, React y Laravel como frameworks</p>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
